c4057bfddd41539f9d57ac2b165ce3314d6c3f95
Jose-Santa <83229693+ElSanta117@users.noreply.github.com>
2021-05-18 16:28:46 -0500

    Add files via upload

---
+++ PryPantallaInicio.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stilos-pagina.css"> 
+    
+    <title>Document</title>
+</head>
+<body>
+    
+    <H1>Youschooline</H1>
+    
+    <h2>¡Sin miedo al exito!</h2><br></br>
+    
+    <div id="header">
+    
+      <ul class="nav">
+          
+        <li><a href=""> Registrarse</a>
+             <ul>
+                 <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/IngresoSys.php"> Docente </a></li>
+                 <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/Datos.php">Estudiante</a></li>
+             </ul>
+             
+        </li></br><br></br>
+        <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/IngresoSys.php">Ingresar</a> </li> <br><br><br><br><br>
+        
+    </div>
+<div id="materias">
+
+        <ul class="mat">
+            <li><a href="">Matemáticas</a>
+            <li><a href="">Lenguaje</a>
+            <li><a href="">Sociales</a>
+            <li><a href="">Filosofía</a>
+            <li><a href="">Inglés</a>
+            <li><a href="">Pastoral</a>
+            <li><a href="">Español</a>
+
+        </ul>
+ 
+</div>   
+
+</body>
+</html>
