55fa1d36e90413374b310e6eda993bcf47b420cc
Jose-Santa <83229693+ElSanta117@users.noreply.github.com>
2021-05-18 14:45:54 -0500

    Add files via upload

---
+++ IngresoSys.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="x-UA-compatibñe" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Contenedor1.css">
+
+
+  <title>Document</title>
+</head>
+
+<body>
+  <form method="post">
+    <p>
+    <h1 class="titulo">Youschooline</h1>
+    </p>
+<br><br>
+    <section class="estilo-registro">
+      
+      <h2 style="text-align: center; background:none;">Ingresar</h2>
+
+      <br><br>
+      <input class="control" name="correo" type="text" placeholder="digite su correo electronico">
+      <br><br>
+
+      <input class="control" name="contraseña" type="password" placeholder="digite su contraseña">
+      <br><br>
+      <br><br>
+
+      <p style="text-align: center; background: none;"><button class="botons" type="submit" name="register">INGRESAR</button><br><br><br><br>
+      <p>
+  </form>
+</body>
+
+</html>
---
+++ PryPantallaInicio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stilos-pagina.css"> 
+    
+    <title>Document</title>
+</head>
+<body>
+    
+    <H1>Youschool</H1>
+    
+    <h2>¡Sin miedo al exito!</h2><br></br>
+    
+    <div id="header">
+    
+      <ul class="nav">
+          
+        <li><a href=""> Registrarse</a>
+             <ul>
+                 <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/IngresoSys.php"> Docente </a></li>
+                 <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/Datos.php">Estudiante</a></li>
+             </ul>
+             
+        </li></br><br></br>
+        <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/IngresoSys.php">Ingresar</a> </li> <br>
+        
+    </div>
+    <img src="https://localhost/CursoPHP/01.introduccionPHP/imagenes/coete.png" width="400" height="500">
+    <p1>Matemáticas</p1>
+    <p2>Lenguaje</p2>
+    <p3>Sociales</p3>
+    <p4>Filosofía</p4>
+    <p5>Inglés</p5>
+    
+
+</body>
+</html>
