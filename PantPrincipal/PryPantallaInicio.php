acbaca592fb2fab86b700bd84a5fb24a9f14a565
Jose-Santa <83229693+ElSanta117@users.noreply.github.com>
2021-06-08 13:19:59 -0500

    Add files via upload

---
+++ PantPrincipal/PryPantallaInicio.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stilos-pagina.css"> 
+    
+    <title>Document</title>
+</head>
+<body>
+    
+    <H1>Youschooline</H1>
+    
+    <p class="po">¡Sin miedo al exito!</p><br></br>
+    
+    <div id="header">
+    
+      <ul class="nav">
+          
+        <li><a href=""> Registrarse</a>
+             <ul>
+                 <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/IngresoSys.php"> Docente </a></li>
+                 <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/Datos.php">Estudiante</a></li>
+             </ul>
+             
+        </li></br><br></br>
+        <li><a href="https://localhost/CursoPHP/01.introduccionPHP/Project/IngresoSys.php">Ingresar</a> </li> <br><br><br><br><br><br>
+        
+    </div>
+<div id="materias">
+
+        <ul class="mat">
+            <li><a href="">Matemáticas</a>
+            <li><a href="">Lenguaje</a>
+            <li><a href="">Sociales</a>
+            <li><a href="">Filosofía</a>
+            <li><a href="">Inglés</a>
+            <li><a href="">Pastoral</a>
+            <li><a href="">Español</a>
+
+        </ul>
+ 
+</div>   
+    <div class="title1">
+    Una nueva forma de aprender
+    </div>
+    <br>
+<p class="deco">Aprender con Youschool es bastante util y eficiente. <br>Poder chequear tus asignaturas y lograr tus objetivos</p>
+<br><br><br><br>    
+<div class="title2">El conociemiento viene, la sabiduria se queda.</div>
+<br><br><br><br>
+    <div class="parrafo">
+        <p2>APREDIZAJE CONTINUO</p2> 
+        <br><br>
+            <p3>
+                Puedes acceder a tus asignaturas y observar todos los temas que se envidencian durante el año académico que cursas.
+            </p3>
+    </div>
+ 
+    <div class="parrafo1">
+
+    <p4>REFORZAR MEDIANTE PRUEBAS</p4>
+    <br><br>
+    <p5>
+    Puedes ver guías y realizarlas para no olvidar aspectos importantes de los temas que tratandurante los periodos acádemicos asignadas por tus maestros.
+    </p5>
+    </div>
+    
+    <div class="imagen23">
+
+        <img src="./imag/cohetebar.jpg" alt="idea 1" class="idea1"></div>
+    
+    <div class="parrafo2">
+    
+        <p6>REPASOS</p6>
+            <br><br>
+            <p7>
+                Se mantienen los temas del año separado por periodos, lo cual ayuda a estar más preparado o capacitado para responder evalucaiones o guías
+            </p7>
+    </div>
+    <div class="parrafo3">
+        <p8>APREDIZAJE PERSONALIZADO</p8>
+        <br><br>
+        <p9>
+        Las lecciones de you school line se adaptan a tu estilo de aprendizaje. 
+        Los ejercicios estan diseñados a tu medida pra ayudarte aprender y repasar las tareas que te han dejado en clase.
+        </p9>
+       
+    </div>
+    <br><br><br><br>
+</body>
+</html>
